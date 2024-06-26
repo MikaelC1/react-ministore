bcdc369db8e74a45bb063df9865a45da448330f4
Manuel Cruz <amcp.negocios@gmail.com>
2024-06-26 09:57:45 -0600

    feat: updated HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom"
 
 export default function HomePage() {
     return (
-        <div>
-            <h1 className="text-4xl font-bold text-center">Home Page</h1>
-            <p className="text-center">This is the home page</p>
-            <div>
-                <Link to='/login'>Login</Link>
-                <Link to='/productos'>Productos</Link>
-                <Link to='/producto/123'>Producto123</Link>
+        <main className="w-full min-h-screen grid place-items-center">
+            <div className="grid gap-2"> 
+                <h1 className="text-4xl font-bold text-center">El Tianguis Libre</h1>
+                <p className="text-center">Mas barato que Temu</p>
+                <div className="flex flex-col items-center gap-4">
+                    <Link to='/login' className="bg-sky-400 rounded w-32 text-center py-2 text-xl cursor-pointer hover:bg-sky-700" >Login</Link>
+                    <Link to='/productos' className="bg-sky-400 rounded w-32 text-center py-2 text-xl cursor-pointer hover:bg-sky-700">Productos</Link>
+                </div>
+
             </div>
-        </div>
+        </main>
     )
 }
